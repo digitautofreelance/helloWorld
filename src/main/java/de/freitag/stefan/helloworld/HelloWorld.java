e6400b979be68efa22a205a622c612834e245f80
Stefan Freitag <stefan.freitag@udo.edu>
2019-03-02 19:16:51 +0100

    Initial commit

---
+++ src/main/java/de/freitag/stefan/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package de.freitag.stefan.helloworld;
+
+public final class HelloWorld {
+
+    private HelloWorld(){
+        //empty method
+    }
+
+    /**
+     * Entry point of this application.
+     *
+     * @param args The command line arguments.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
